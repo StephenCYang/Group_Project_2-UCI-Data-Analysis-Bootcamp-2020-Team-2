c6f3359c75cd3866284f401e2d2ab07bf2011c24
Stephen Yang <26841782+StephenCYang@users.noreply.github.com>
2020-07-13 15:10:48 -0700

    added notes

--- sankey/sankeyGraph.js
+++ sankey/sankeyGraph.js
@@ -3,6 +3,8 @@
 //Load the json from https://fantasy.premierleague.com/api/bootstrap-static/
 var link="https://fantasy.premierleague.com/api/bootstrap-static/";
 
+
+// The following snippet is from https://stackoverflow.com/questions/43262121/trying-to-use-fetch-and-pass-in-mode-no-cors/43268098
 var proxyUrl = 'https://cors-anywhere.herokuapp.com/',
     targetUrl = link
 fetch(proxyUrl + targetUrl)
